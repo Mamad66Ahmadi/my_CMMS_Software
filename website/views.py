c8d148f937159de1aa6189bc439cd8aff3373cbf
Seyed Mohammad Ahmadi <106057277+Mamad66Ahmadi@users.noreply.github.com>
2026-02-26 16:29:07 +0330

    created first app

---
+++ website/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index_view(request):
+    return HttpResponse('<h1> Home page </h1>')
+
+def contact_view(request):
+    return HttpResponse('<h1> Contact page </h1>')
